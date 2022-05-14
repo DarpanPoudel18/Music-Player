969b98c42eddb594bbb813a43125969e44afc882
Darpan <darpanpaudel1998@gmail.com>
2022-05-14 09:42:12 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import Home from './Components/Home'
+import './App.css'
 
-function App() {
+const App = () => {
+  const[songs,setSongs] = useState(
+    [
+      {
+        id:1,
+        title:"Tere Liye",
+        artist:"atif aslam",
+        img_src:"./images/tereliye.jpg ",
+        src:"./songs/TereLiye.mp3"
+      },
+      {
+        id:2,
+        title:"Tera Hone Laga",
+        artist:"atif aslam",
+        img_src:"./images/terahonelaga.jpg",
+        src:"./songs/TeraHoneLaga.mp3"
+      },
+      {
+        id:3,
+        title:"Bakhuda",
+        artist:"atif aslam",
+        img_src:"./images/bakhudatum.jpg",
+        src:"./songs/Bakhuda.mp3"
+      },
+      {
+        id:4,
+        title:"Tu Jane Na",
+        artist:"atif aslam",
+        img_src:"./images/tu.jpg",
+        src:"./songs/TUJaneNa.mp3"
+      },
+      {
+        id:5,
+        title:"Be Intehaan",
+        artist:"atif aslam",
+        img_src:"./images/beintehaan.jpeg",
+        src:"./songs/BeIntehaan.mp3"
+      },
+      {
+        id:6,
+        title:"Doorie",
+        artist:"atif aslam",
+        img_src:"./images/doorie.jpeg",
+        src:"./songs/Doorie.mp3"
+      }
+      
+    ]
+  )
+
+  const[currentSongIndex,setCurrentSongIndex] = useState(0);
+  const[nextSongIndex,setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(()=>{
+    setNextSongIndex(()=> {
+      if(currentSongIndex + 1 > songs.length - 1){
+        return 0;
+      }else{
+        return currentSongIndex + 1;
+      }
+    });
+       },[currentSongIndex]);
+
+
+       const SkipSong=(forwards=true)=>{
+        if(forwards){
+          setCurrentSongIndex(()=>{
+            let temp = currentSongIndex;
+            temp++;
+      
+            if(temp > songs.length - 1){
+              temp = 0;
+            }
+      
+            return temp;
+          })
+        } else{
+          setCurrentSongIndex(()=>{
+            let temp = currentSongIndex;
+            temp--;
+      
+            if(temp < 0){
+              temp = songs.length - 1;
+            }
+      
+            return temp;
+          })
+        }
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App" >
+    
+   <Home 
+    currentSongIndex={currentSongIndex}
+    setCurrentSongIndex={setCurrentSongIndex}
+    nextSongIndex={nextSongIndex}
+    songs={songs}
+    SkipSong={SkipSong}
+    title={songs[nextSongIndex].title}
+   />
+   </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,124 @@
+import React,{useState,useRef,useEffect} from 'react'
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+// import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import {faPlay, faPause, faForward, faBackward} from '@fortawesome/free-solid-svg-icons';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+// const url = ' https://api.npoint.io/50a568126cec34085c17'
+
+const Home = (props) => {
+    const theme = useTheme();
+    const audioEl = useRef(null);
+    const[isPlaying,setIsPlaying] = useState(false);
+    const[rotate,setRotate] = useState(false);
+    const[songs,setSongs]=useState([props.songs])
+    console.log(props);
+    
+
+    useEffect(()=>{
+      if(isPlaying){
+        audioEl.current.play();
+      }else{
+        audioEl.current.pause();
+      }
+      console.log(audioEl);
+      },[isPlaying])
+    
+
+   
+   return (
+    <>
+      <Box className="card-parent container" >
+       <br/>
+       <h1>Playing Now</h1>
+       
+       <hr className="bar"></hr>
+          <CardContent className="card-content" sx={{ flex: '1 0 auto' }}>
+        
+
+             {rotate ?  <CardMedia
+                      className="song-img"
+                       component="img"
+                       sx={{ width: 151 }}
+                       image={ props.songs[props.currentSongIndex].img_src}
+                       alt="error displaying img"
+                       title={ props.songs[props.currentSongIndex].title}
+                       /> : <CardMedia  
+                      className="song-img-before"
+                       image={ props.songs[props.currentSongIndex].img_src}
+                       sx={{ width: 151 }}
+                       > </CardMedia>
+                   }
+
+              <Typography >
+                 <span className="txt-shadow-title">{props.songs[props.currentSongIndex].title }</span>
+              </Typography>
+           
+               <Typography>
+                 <span className="txt-shadow-artist">{props.songs[props.currentSongIndex].artist}</span> 
+                <audio src={props.songs[props.currentSongIndex].src} ref={audioEl}></audio>
+               </Typography>
+
+          </CardContent>
+
+<h2 className="p-3 mb-2 bg-primary text-white" style={{textAlign:'center',color:'white'}}>
+Next:{props.songs[props.nextSongIndex].title }
+</h2>
+
+         <Box className="music-function">
+             <IconButton aria-label="previous" onClick={()=> props.SkipSong(false)}>
+             {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+             </IconButton>
+
+             <IconButton aria-label="play/pause" onClick=
+                 {
+                  ()=> {
+                    setRotate(true); 
+                    setIsPlaying(!isPlaying);
+                   
+                    }
+                 }>
+                 <FontAwesomeIcon icon={isPlaying ? faPause : faPlay}/>
+             </IconButton>
+
+             <IconButton aria-label="next" onClick={()=> {
+               setIsPlaying(!isPlaying);
+                 props.SkipSong()
+                 setRotate(false)
+               }}>
+               {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+             </IconButton>
+
+           
+        </Box>
+  </Box>
+    
+ 
+        
+        
+      
+    
+  </>
+  )
+}
+
+export default Home
+
+
+
+
+
+ 
+ 
+ 
